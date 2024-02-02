ee468a262351d2712af3e28346ef57eae30ce3e3
Denis <netfixk@yandex.ru>
2024-02-02 21:34:51 +0300

    banner dev mode complete

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
             <Container width="1600px">
                 <div className={styles.header_holder}>
                     <Logo width="28px" height="28px"/>
-                    <a className={clsx( main_styles.btn, main_styles.btn__invert)} type="invert">
+                    <a href="https://t.me/FFGloba1" target="_blank" rel="noreferrer" className={clsx( main_styles.btn, main_styles.btn__invert)} type="invert">
                         <span>Рассчитать поставку</span>
                         <svg width="13" height="22" viewBox="0 0 13 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1.66699 2L11.0003 11.3333L1.66699 20.6667" stroke="white" strokeWidth="2.66667"
