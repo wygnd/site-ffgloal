b62be4cbe89025788d27477f1c42a49800c199a4
root <root@2465722-zp65086.twc1.net>
2024-02-23 17:53:05 +0300

    init

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('app') as HTMLElement
+  document.getElementById("global") as HTMLElement
 );
 root.render(
   <React.StrictMode>
