f83f8dc72dab414ae258563bd2fb414593016188
Denis <wygndo@gmail.com>
2024-03-19 10:00:25 +0300

    init new branch

--- client/App.tsx
+++ client/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Layout from "@/components/Layout/Layout";
 import MainPage from "@/pages/mainPage/MainPage";
-import AdminPage from "@/pages/adminPage/AdminPage";
 
 
 const App = () => (
@@ -10,7 +9,6 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route path="/" element={<MainPage/>}/>
-        <Route path="/adin" element={<AdminPage/>}/>
       </Route>
     </Routes>
   </>
